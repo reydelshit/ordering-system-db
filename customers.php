d149024fe355da555440bebf161e836071a8f02a
Reydel Ocon <reydel321@gmail.com>
2023-10-20 14:00:45 +0800

    nice nice

---
+++ customers.php
@@ -0,0 +1,31 @@
+<?php
+
+
+include 'DBconnect.php';
+$objDB = new DbConnect();
+$conn = $objDB->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case "GET":
+
+
+        $sql = "SELECT users.user_id, users.name, users.email, users.address, COUNT(orders.user_id) AS orders_count, COUNT(DISTINCT feedback.feedback_id) AS feedback_count
+        FROM users INNER JOIN orders ON orders.user_id = users.user_id
+        INNER JOIN feedback ON feedback.user_id = users.user_id
+        GROUP BY users.user_id";
+
+
+        if (isset($sql)) {
+            $stmt = $conn->prepare($sql);
+
+            $stmt->execute();
+            $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            echo json_encode($customers);
+        }
+
+
+        break;
+}
